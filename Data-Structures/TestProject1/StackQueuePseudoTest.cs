b235f5c9eb833adfacec09a0ee8608c103f952e8
tomcatoo <ahmad.msadae@gmail.com>
2022-04-24 13:00:42 +0300

    resubmit

---
+++ Data-Structures/TestProject1/StackQueuePseudoTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Data_Structures.Data_structures.stack_queue_pseudo;
+
+namespace TestProject1
+{
+    public class StackQueuePseudoTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            StackQueuePseudo sq = new StackQueuePseudo();
+            sq.Enqueue(20);
+            sq.Enqueue(15);
+            sq.Enqueue(10);
+            sq.Enqueue(5);
+            Assert.Equal(20,sq.Dequeue());
+            Assert.Equal(15, sq.Dequeue());
+        }
+    }
+}
