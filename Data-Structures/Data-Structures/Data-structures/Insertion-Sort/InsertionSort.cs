5386fa34419fc82af1704273cf9cd13a963d24e5
Ahmad Masadeh <62568250+MasadehAhmad@users.noreply.github.com>
2022-05-24 22:45:15 +0300

    challeng 26

---
+++ Data-Structures/Data-Structures/Data-structures/Insertion-Sort/InsertionSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures.Data_structures.Insertion_Sort
+{
+    public class InsertionSort
+    {
+        public static void Insertion_Sort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 1; i < n; i++)
+            {
+                int j = i - 1;
+                int temp = arr[i];
+
+                while (j >= 0 && arr[j] > temp)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = temp;
+            }
+        }
+        
+    }
+}
