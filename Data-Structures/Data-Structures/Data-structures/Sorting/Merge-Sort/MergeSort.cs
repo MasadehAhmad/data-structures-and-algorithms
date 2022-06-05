733427e9c3cde3701a152656005d325f4837c9ab
Ahmad Masadeh <62568250+MasadehAhmad@users.noreply.github.com>
2022-06-05 11:59:35 +0300

    Merge Sort

---
+++ Data-Structures/Data-Structures/Data-structures/Sorting/Merge-Sort/MergeSort.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures.Data_structures.Sorting.Merge_Sort
+{
+    public class MergeSort
+    {
+        public static int[] MergesortMethod(int[] arr)
+        {
+            int n = arr.Length;
+            if (n > 1)
+            {
+                int mid = n / 2;
+                int[] left = arr.Take(mid).ToArray();
+                int[] right = arr.Skip(mid).ToArray();
+                MergesortMethod(left);
+                MergesortMethod(right);
+                Merge(left, right, arr);
+            }
+            return arr;
+        }
+
+        public static void Merge(int[] left, int[] right, int[] arr)
+        {
+            int i = 0;
+            int j = 0;
+            int k = 0;
+
+            while (i < left.Length && j < right.Length)
+            {
+                if (left[i] <= right[j])
+                {
+                    arr[k] = left[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (j < right.Length)
+            {
+                arr[k] = right[j];
+                j++;
+                k++;
+            }
+
+            while (i < left.Length)
+            {
+                arr[k] = left[i];
+                i++;
+                k++;
+            }
+        }
+    }
+}
