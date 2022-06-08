3bd2188ae6d3db499ff1ec3a42c33c208f7dd3ee
Ahmad Masadeh <62568250+MasadehAhmad@users.noreply.github.com>
2022-06-08 21:58:03 +0300

    add Test

--- Data-Structures/Data-Structures/Program.cs
+++ Data-Structures/Data-Structures/Program.cs
@@ -11,22 +11,9 @@ namespace Data_Structures
     {
         static void Main(string[] args)
         {
-            TreeNode root = new TreeNode("15");
-            root.children.Add(new TreeNode("2"));
-            root.children.Add(new TreeNode("3"));
-            root.children.Add(new TreeNode("4"));
-            root.children[0].children.Add(new TreeNode("5"));
-            root.children[0].children.Add(new TreeNode("6"));
-            root.children[0].children.Add(new TreeNode("7"));
-            root.children[1].children.Add(new TreeNode("8"));
-            root.children[2].children.Add(new TreeNode("9"));
-            root.children[2].children.Add(new TreeNode("10"));
-            root.children[2].children.Add(new TreeNode("11"));
-            List<string> list = root.traverse(root);
-            foreach(string s in list) Console.WriteLine(s);
-            Console.WriteLine("");
-            List<string> list2 = root.FizzBuzz(root);
-            foreach (string s in list2) Console.WriteLine(s);
+           List<int> list = new List<int>();
+            list[5] = 1;
+            Console.WriteLine(list[5]);
         }
     }
 }
---
+++ Data-Structures/TestProject1/HashTable-Test.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Data_Structures.Data_structures.hashtable;
+namespace TestProject1
+{
+    public class HashTable_Test
+    {
+        [Fact]
+        public void add_get_Test()
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.set(5, 522);
+            hashtable.set(4, 365);
+            Assert.Equal( 522,hashtable.get(5));
+        }
+        [Fact]
+        public void keys_Test()
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.set(5, 522);
+            hashtable.set(4, 365);
+            hashtable.set(88, 365);
+            hashtable.set(77, 85596);
+            Assert.Equal(new List<int> { 88, 77, 4, 5 }, hashtable.keys());
+        }
+        [Fact] 
+        public void containsTest()
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.set(5, 522);
+            hashtable.set(4, 365);
+            hashtable.set(88, 365);
+            hashtable.set(77, 85596);
+            Assert.True(hashtable.contains(4));
+            Assert.True(hashtable.contains(88));
+            Assert.False(hashtable.contains(66));
+        }
+    }
+}
