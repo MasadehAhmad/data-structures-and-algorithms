9fc452fbf6ce5446e783c137a2df8f9c8f289dca
Ahmad Masadeh <62568250+MasadehAhmad@users.noreply.github.com>
2022-06-24 07:24:17 +0300

    tree_intersection

---
+++ Data-Structures/Data-Structures/Data-structures/tree-intersection/TreeIntersection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data_Structures.Data_structures.Trees;
+
+namespace Data_Structures.Data_structures.tree_intersection
+{
+    public class TreeIntersection
+    {
+        public List<int> Intersection(BinarySearchTree tree1 , BinarySearchTree tree2)
+        {
+            List<int> result = new List<int>();
+            var tree1List = tree1.InOrder(tree1.Root);
+            var tree2List = tree2.InOrder(tree2.Root);
+            foreach (int i in tree1List)
+            {
+                if(tree2List.Contains(i)) result.Add(i);
+                
+            }
+            return result;
+        }
+    }
+}
